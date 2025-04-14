8489114b46c5fac4d415657aabf6a513c36c5660
mbasaran96 <mert.basaran96@yahoo.com>
2025-04-14 12:09:09 +0200

    Python Script_Data Dictionary_SQL Create

---
+++ Python Scripts/datadict2sqlcreate.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from icecream import ic
+
+def write_sqlcreate_for_table(table: pd.DataFrame, fk_dict: dict):
+    """Writes SQL Create statement for one table."""
+    table_name = table.Tablename.unique()[0]
+    create_statement = f'-- Tabelle {table_name} erstellen.\n'
+    create_statement += f'DROP TABLE IF EXISTS {table_name};\n'
+    create_statement += f'CREATE TABLE {table_name} (\n'
+    pks = []
+    fks = []
+    for row in table.index:
+        #ic(row)
+        if table.loc[row, "Spaltentyp"].upper() in ['VARCHAR', 'TEXT']:
+            row_sql = f'{table.loc[row, "Spaltenname"]} {table.loc[row, "Spaltentyp"].upper()} ({int(table.loc[row, "Zeichenlaenge"])}),\n'
+        elif table.loc[row, "Spaltentyp"].upper() == 'DECIMAL':
+            row_sql = f'{table.loc[row, "Spaltenname"]} {table.loc[row, "Spaltentyp"].upper()} ({table.loc[row, "Zeichenlaenge"]}),\n'
+        else:
+            row_sql = f'{table.loc[row, "Spaltenname"]} {table.loc[row, "Spaltentyp"].upper()},\n'
+        if table.loc[row, 'PK'] == 'PK':
+            pks.append(table.loc[row, "Spaltenname"])
+        if table.loc[row, 'FK'] == 'FK':
+            fks.append(table.loc[row, "Spaltenname"])
+        create_statement += row_sql
+    for fk in fks:
+        fk_sql = f'FOREIGN KEY ({fk}) REFERENCES {fk_dict[fk]}({fk}),\n'
+        create_statement += fk_sql
+    if len(pks) == 1:
+        create_statement += f'PRIMARY KEY ({pks[0]} AUTOINCREMENT)\n'
+    else:
+        create_statement += f'PRIMARY KEY ({", ".join(pks)})\n'
+    create_statement += ');\n\n'
+    return create_statement
+
+def get_primary_table_for_FK(data_dict: pd.DataFrame, FK_name: str) -> str:
+    """Returns primary table for given FK. If no table is found raises error."""
+    prim_table = data_dict.loc[(data_dict.Spaltenname == FK_name) & (data_dict.PK == 'PK') & (data_dict.FK != 'FK') , 'Tablename']
+    if len(prim_table.values) != 1:
+        ic(f'Check FK  - PK naming for {FK_name}. {len(prim_table.values)}')
+    return prim_table.values[0] 
+
+def build_FK_dict(data_dict: pd.DataFrame) -> dict:
+    """Function to collect primary table for Foreign Keys and save into dict."""
+    with_FK = data_dict.loc[data_dict.FK == 'FK']
+    list_of_fks = with_FK.Spaltenname.unique()
+    fk_prim_table = {}
+    for fk in list_of_fks:
+        fk_prim_table[fk] = get_primary_table_for_FK(data_dict, fk)
+    return fk_prim_table
+
+def write_create_table_statements(data_dict: pd.DataFrame, dd_name: str):
+    """Writes all create table statements to sql file."""
+    fk_dict = build_FK_dict(data_dict)
+    with open(f"create_tables_{dd_name}.sql", 'w') as w_file:
+        for t_name in data_dict.Tablename.unique():
+            table_df = data_dict.loc[data_dict.Tablename == t_name]
+            w_file.write(write_sqlcreate_for_table(table_df, fk_dict ))
+
+def main():
+    pass
+
+if __name__=='__main__':
+    datadict_path = 'Spielwarenläden_v2.csv' # Name vom Datadict
+    dd_name = datadict_path.split('/')[-1][:-4]
+    data_dict = pd.read_csv(datadict_path)
+    write_create_table_statements(data_dict, dd_name)
+    main()
---
+++ Python Scripts/xml2datadict.py
@@ -0,0 +1,94 @@
+# Import libraries
+from icecream import ic # debugging Tool
+import re # analyses XML strings
+import xml.etree.ElementTree as ET # parses XML file
+import pandas as pd # creates data frame for data dictionary
+from html.parser import HTMLParser # parses HTML
+
+# Define Class to parse HTML
+class HTMLTextExtractor(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.fed = [] # cache for real text
+
+    def handle_data(self, d):
+        self.fed.append(d) # is called when plain text is found between tags
+
+    def get_data(self):
+        return ''.join(self.fed) # returns the collected text as a string
+
+# Create Functions
+def strip_html(html):
+    """Removes HTML-Tags and returns clear text."""
+    parser = HTMLTextExtractor()
+    parser.feed(html)
+    return parser.get_data()
+
+def list_cells(xml_path: str):
+    """Function to extract all cells into list."""
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    cells = root.findall('./diagram/mxGraphModel/root/mxCell') # './mxCell'
+    return cells 
+
+def get_shape_from_cell(cell):
+    """Extract shape property from style attribute"""
+    style = cell.get('style')
+    prop=None
+    if isinstance(style, str):
+        prop = re.match(r'shape=([a-zA-Z]+);', style)
+    if prop is not None:
+        return prop.group(1)
+    else:
+        return None
+
+def create_datadf(table_collect: dict):
+    """Create blueprint of data dict."""
+    d_dict = pd.DataFrame(columns=['Tabellenname', 'Spaltenname', 'Spaltentyp', 'Zeichenlaenge', 'PK', 'FK', 'AI', 'NN', 'Beschreibung'])
+    row = 0
+    for table_name, table in table_collect.items():
+        for erm_table_row in range(int(len(table)/2)):
+            flag = table[2*erm_table_row]
+            description = table[2*erm_table_row + 1]
+            if flag.startswith('PK,FK'):
+                d_dict.loc[row] = [table_name, description, '', '', 'PK', 'FK', '', 'NN', '']
+            elif flag == 'PK':
+                d_dict.loc[row] = [table_name, description, '', '', flag, '', 'AI', 'NN', '']
+            elif flag == 'FK':
+                d_dict.loc[row] = [table_name, description, '', '', '', flag, '', '', '']
+            else:
+                d_dict.loc[row] = [table_name, description, '', '', '', '', '', '', '']
+            row += 1
+           
+    return d_dict
+
+def get_table_dict(cell_list: list):
+    """Extract information needed for table, with HTML cleaned."""
+    table_collect = {}
+    table_name = None
+    for cell in cell_list:
+        shape = get_shape_from_cell(cell)
+        value = cell.get('value')
+        if value:
+            value = strip_html(value).strip()
+        if shape == 'table':
+            table_name = value
+            table_collect[table_name] = []
+        elif shape == 'partialRectangle':
+            if table_name is not None:
+                table_collect[table_name].append(value)
+    return table_collect
+
+# Main Function to parse, extract and export as csv file
+def main():
+    xml_path = '/Users/mertali/Desktop/Coding Challenge/SnowflakeSchema.xml'
+    name = xml_path[:-4]
+    cells = list_cells(xml_path)
+    cell_dict = get_table_dict(cells)
+    data_dict_df = create_datadf(cell_dict)
+    data_dict_df.to_csv(f'{name}.csv', index=False)
+    ic(data_dict_df)
+
+if __name__=='__main__':
+    main()
